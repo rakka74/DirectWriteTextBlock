f33b2e535b02b20c87dcf42b0c9ed041261f6acc
rakka74 <rakka74@gmail.com>
2016-12-08 03:31:59 +0900

    WPFDXInteropの描画メソッド実装用のC++/CLIのクラスライブラリのプロジェクトを追加。

---
+++ DirectWriteTextBlockApp/DirectWriteTextBlockLib/DirectWriteTextBlockLib.h
@@ -0,0 +1,13 @@
+// DirectWriteTextBlockLib.h
+
+#pragma once
+
+using namespace System;
+
+namespace DirectWriteTextBlockLib {
+
+	public ref class Class1
+	{
+		// TODO: このクラスの、ユーザーのメソッドをここに追加してください。
+	};
+}
